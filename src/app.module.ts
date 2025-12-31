b4fcd0feb53af7ad66bc63adfddb7d1733aa8001
Rishabh-Pandey007 <rishabhpandey2023@gmail.com>
2026-01-01 00:42:40 +0530

    feat: create auth, user, workspace and project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, UsersModule, WorkspacesModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
