1691d0dca421793ea97740f5fcb4cfde20902d54
Rishabh-Pandey007 <rishabhpandey2023@gmail.com>
2026-01-01 14:58:31 +0530

    feat:P inialized projects, task, comment, file and notifications modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { UserModule } from './user/user.module';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
+import { FilesModule } from './files/files.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [AuthModule, UserModule, UsersModule, WorkspacesModule, ProjectsModule],
+  imports: [AuthModule, UserModule, UsersModule, WorkspacesModule, ProjectsModule, TasksModule, CommentsModule, FilesModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
